f1cca9d6edf8ae59f4c9f50fb1d427c63b759169
AyushSths <ayushsthx088@gmail.com>
2024-01-30 11:20:28 +0545

    key frames

--- job-application/src/components/Navbar.jsx
+++ job-application/src/components/Navbar.jsx
@@ -18,12 +18,13 @@ function Navbar({ setCondition }) {
             <div className="navibar">
                 <nav className={`navbar navbar-expand-lg navbar-light ${isNotHome ? 'bg-grey' : ''}`}>
                     <div className="container-fluid">
-                        <NavLink className="navbar-brand" to="/" style={{ color: "#198745", fontWeight: "700" }}>Jobs Portal</NavLink>
+                        {/* <NavLink className="navbar-brand" to="/" style={{ color: "#198745", fontWeight: "700" }}>Jobs Portal</NavLink> */}
+                        <NavLink className={`navbar-brand ${isNotHome ? '' : 'logo'}`} to="/" style={{ color: "#198745", fontWeight: "700" }}>Jobs Portal</NavLink>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <ul className={`navbar-nav me-auto mb-2 mb-lg-0 ${isNotHome ? '' : 'items'}`}>
                                 <li className="nav-item">
                                     <NavLink className="nav-link" aria-current="page" to="/">Home
                                         <span className='line'></span>
@@ -71,7 +72,7 @@ function Navbar({ setCondition }) {
                         </div>
                     </div>
                 </nav>
-            </div>
+            </div >
         </>
 
     )
