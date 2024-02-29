9fcf59e90b07e89e9cefd9c6e24d506a3d974696
AyushSths <123633939+AyushSths@users.noreply.github.com>
2024-02-29 11:44:33 +0545

    Delete job-application directory

--- job-application/src/es-module/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const db = "mongodb+srv://ayushsthx088:mongo3d2y@cluster0.nmse8gp.mongodb.net/portal-db?retryWrites=true&w=majority"
-
-mongoose.connect(db).then(() => {
-    console.log("Connection successful");
-}).catch((err) => {
-    console.log("Error while connecting", err)
-})
